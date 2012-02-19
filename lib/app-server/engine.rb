57c06b64d2e08ba077342126504eb67328e8c9d5
LukeHammond <luke.e.j.hammond@gmail.com>
2012-02-19 13:34:30 -0500

    add comments about auto_extendibility

--- lib/app-server/engine.rb
+++ lib/app-server/engine.rb
@@ -1,6 +1,8 @@
 module AppServer
   class Engine < Rails::Engine
-    config.after_initialize do 
+    config.after_initialize do
+      # After rails has been initialized require all the model_extensions that will be used to augment the models
+      # define in the gem 
       Dir[Rails.root.join('lib', 'model_extensions', '*.rb').to_s].each {|file| require file }
     end
   end
--- lib/auto_extendible/base.rb
+++ lib/auto_extendible/base.rb
@@ -1,3 +1,40 @@
+# The purpose of this module is to provide a mechanism for additional functionality
+# to be added to a model defined within the engine
+
+# To use, flag the model in the engine as auto_extendible
+
+# class Wine < ActiveRecord::Base
+#   auto_extendible
+# end
+
+# Within the app that hosts the engine create a directory under the lib directory
+# called module_extensions. Then create a module using this example as a template
+
+# module ModelExtensions
+#   module Wine
+#     extend ActiveSupport::Concern
+# 
+#     included do
+# 
+#       has_one :cheese, :as => :api
+#     end
+#     
+#     module InstanceMethods
+#       def poor
+#         puts "glug glug glug"
+#       end
+#     end
+#     
+#     module ClassMethods
+#       def label
+#         puts "fancy label"
+#       end
+#     end
+#   end
+# end
+
+
+
 module AppServer
   module AutoExtendible
 
