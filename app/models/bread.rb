81561eaba7b0e4232cfd1d0bf1f85a82f0665b67
LukeHammond <luke.e.j.hammond@gmail.com>
2012-02-20 15:58:09 -0500

    add prepare method to bread rspec and model

--- app/models/bread.rb
+++ app/models/bread.rb
@@ -1,5 +1,11 @@
 class Bread < ActiveRecord::Base
+  auto_extendible
+
   self.table_name = 'bread'
   
   has_many :meals
+
+  def prepare
+    puts "slicing"
+  end
 end
--- spec/models/bread_spec.rb
+++ spec/models/bread_spec.rb
@@ -4,4 +4,5 @@ describe Bread do
   subject { FactoryGirl.build(:bread) }
 
   it { should have_many(:meals) }
+  it { should respond_to(:prepare) }
 end
