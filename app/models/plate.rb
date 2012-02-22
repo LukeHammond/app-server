381ce5e1890790d34bb13dabfc43b8b41dd8b56e
LukeHammond <luke.e.j.hammond@gmail.com>
2012-02-21 19:23:05 -0500

    update to latest use of auto-observer gem

--- app/models/plate.rb
+++ app/models/plate.rb
@@ -1,18 +1,11 @@
 class Plate
-  extend ActiveModel::Callbacks
 
   CALLBACKS = [:clean]
-  
-  define_model_callbacks *CALLBACKS
-  
+
   def clean
     puts "I am being scrubbed good and squeaky"
   end
-end
 
-Plate.class_eval do
-  include AutoObserver::Observing if defined?(AutoObserver::Observing)
-  include AutoObserver::Callbacks if defined?(AutoObserver::Callbacks)
+  include AutoObserver
 end
-
   
